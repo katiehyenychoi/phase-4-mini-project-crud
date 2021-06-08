87b8a09779aa08a73352bd44b285d9228b512546
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-06-08 14:12:45 -0400

    commit tree fe7f04980ee7ddb4d7f8f8bad67bf6fb6a10e761

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,38 @@
+class SpicesController < ApplicationController
+  def index
+    spices = Spice.all
+    render json: spices
+  end
+
+  def show
+    spice = Spice.find_by(id: params[:id])
+    render json: spice
+  end
+
+  def create
+    spice = Spice.create(spice_params)
+    render json: spice, status: :created
+  end
+
+  def update
+    spice = Spice.find_by(id: params[:id])
+    spice.update(spice_params)
+    render json: spice
+  end
+
+  def destroy
+    spice = Spice.find_by(id: params[:id])
+    spice.destroy
+    render json: "Spice deleted"
+  end
+
+  private
+
+  def spice_params
+    params.permit(:title, :image, :description, :notes, :rating)
+  end
+
+  def spice_params
+    params.permit(:title, :image, :description, :notes, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get "/spices", to: "spices#index"
+  # get "/spices/:id", to: "spices#show"
+  post "/spices", to: "spices#create"
+  patch "/spices/:id", to: "spices#update"
+  delete "/spices/:id", to: "spices#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
